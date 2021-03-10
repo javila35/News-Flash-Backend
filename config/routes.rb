46ebd402bec33f1ab64dfaf2e046a272228a4ed7
javila35 <josepharthuravila@gmail.com>
2021-03-10 11:25:15 -0800

    Update routes Allow users#show to use username param All other users routes uses id

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :comments
   resources :bookmarks
-  resources :users, param: :username
+  resources :users, except: :show
+  get "users/:username", to: "users#show"
   post '/auth', to: "auth#create"
   get '/current_user', to: 'auth#show'
 end
