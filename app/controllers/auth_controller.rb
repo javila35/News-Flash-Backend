4b9ecbb12df57741a21a55fb96d8ff98b76f0e5c
javila35 <josepharthuravila@gmail.com>
2021-04-03 12:57:31 -0700

    Authenitcation errors Add more detailed errors to auth process

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,59 +1,73 @@
 class AuthController < ApplicationController
-    def create
-        user = User.find_by(username: auth_params[:username])
-        if user && user.authenticate(auth_params["password"])
-          token = issue_token(user)
-          render json: {
-            user: {
-              id: user.id, 
-              username: user.username, 
-              first_name: user.first_name, 
-              bio: user.bio, 
-              location: user.location,
-            },
-              jwt: token,
-              status: 202
-          }
-        else
-          render json: {
-            response: 'Log in failed.', 
-            status: 401
-          }
-        end
-      end
+  def create
+    user = User.find_by(username: auth_params[:username])
+    if !user
+      render json: {
+               error: "Could not find username.",
+               status: 401,
+             }
+      return
+    elsif user && !user.authenticate(auth_params[:password])
+      render json: {
+               error: "Incorrect password",
+               status: 401,
+             }
+    elsif user && user.authenticate(auth_params[:password])
+      token = issue_token(user)
+      render json: {
+               user: {
+                 id: user.id,
+                 username: user.username,
+                 first_name: user.first_name,
+                 bio: user.bio,
+                 location: user.location,
+               },
+               jwt: token,
+               status: 202,
+             }
+      return
+    else
+      render json: {
+        error: "Unknown error.",
+        status: 401,
+      }
+      return
+    end
+  end
 
-      def show
-        # Get token from request
-        token = request.authorization
+  def show
+    # Get token from request
+    token = request.authorization
 
-        # Decode token
-        response  = JWT.decode(token, Rails.application.secret_key_base, true, { :algorithm => 'HS256' })
+    # Decode token
+    response = JWT.decode(token, Rails.application.secret_key_base, true, { :algorithm => "HS256" })
 
-        # Destructuring decoded token
-        user_id = response[0]["user_id"]
+    # Destructuring decoded token
+    user_id = response[0]["user_id"]
 
-        user = User.find(user_id)
-        if user
-          render json: {
-            user: {
-              id: user.id,
-              username: user.username,
-              first_name: user.first_name,
-              bio: user.bio,
-              location: user.location,
-            },
-            status: 200
-        }
-        else
-          render json: {
-            error: "Token could not be decoded.",
-            status: 401
-          }
-        end
-      end
+    user = User.find(user_id)
+    if user
+      render json: {
+               user: {
+                 id: user.id,
+                 username: user.username,
+                 first_name: user.first_name,
+                 bio: user.bio,
+                 location: user.location,
+               },
+               status: 200,
+             }
+    else
+      render json: {
+               error: "Token could not be decoded.",
+               status: 401,
+             }
+    end
+  end
 
-      private
-      def auth_params
-        params.require(:auth).permit(:username, :password)
-      end
+  private
+
+  def auth_params
+    params.require(:auth).permit(:username, :password)
+  end
 end
